9658518e65096a8d9b1ce3ed6e6f37260d35949d
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-01-29 19:36:34 +0000

    Implementation of Relationship between our Models

--- app/Product.php
+++ app/Product.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App;
+use App\Category;
+use App\Seller;
+use App\Transaction;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,15 +11,34 @@ class Product extends Model
 {
     //Constant to check status
     const AVAILABLE_PRODUCT = 'available';
-    const UNVAILABLE_PRODUCT = 'unavalable';
+    const UNVAILABLE_PRODUCT = 'unavailable';
 
     protected  $fillable = ['name', 'description', 'quantity', 'status','image', 'seller_id'];
 
 
     //Product is available
 
-    public function  isAvalable()
+    public function  isAvailable()
     {
         return $this->status == Product:: AVAILABLE_PRODUCT;
     }
+
+    public function  transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function  seller()
+    {
+       return $this->belongsTo(Seller::class) ;
+    }
+
+
+    public  function  categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+
+
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App;
-
+use  App\Product;
+use App\Buyer;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
@@ -9,4 +10,14 @@ class Transaction extends Model
     //
 
     protected  $fillable = ['quantity', 'buyer_id', 'product_id'];
+
+    public  function  buyer()
+    {
+        return $this->belongsTo(Buyer::class);
+    }
+
+    public  function  product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
