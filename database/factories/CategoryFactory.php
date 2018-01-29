6b061a64c2f033e79ddbb7646fdbe9071f3ba3e2
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-01-29 21:12:16 +0000

    Product Factory has been created

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 $factory->define(Category::class, function (Faker $faker) {
     return [
         //
-          'name ' => $faker -> word ,
+           'name' => $faker -> word ,
            'description' => $faker->paragraph(1),
     ];
 });
