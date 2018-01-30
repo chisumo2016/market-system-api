b9cc513f0e8e12ccf2edebafad528dbef81d6768
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-01-30 21:48:00 +0000

    Handlind MethodNotAllowedHttpException has been implemented

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
@@ -74,6 +75,10 @@ class Handler extends ExceptionHandler
 
         if($exception instanceof  NotFoundHttPException){ // Handle the Authorization
             return $this->errorResponse("The specified URL cannot be found ", 404);
+        }
+
+        if($exception instanceof  MethodNotAllowedHttpException){ // Handle the Authorization
+            return $this->errorResponse("The specified method for the request is invalid ", 405);
         }
             return parent::render($request, $exception);
     }
