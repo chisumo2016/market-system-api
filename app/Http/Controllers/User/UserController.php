006d1eb1466d90a131fede825228861b81fdb22c
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-01-30 17:50:09 +0000

    Using the Generalize Methods for Error Response has implemented Successfully

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -121,8 +121,8 @@ class UserController extends ApiController
 
         if ($request->has('admin')){
             if (!$user ->isVerified()) {
-
-                return response()->json(['error' => 'Only verified users can modify the admin field ' , 'code' => 409], 409 );
+                    return $this->ErrorResponse('Only verified users can modify the admin field ', 409);
+                //return response()->json(['error' => 'Only verified users can modify the admin field ' , 'code' => 409], 409 );
             }
 
             // Assign the value
@@ -130,7 +130,8 @@ class UserController extends ApiController
         }
         // Any changes in the model
         if (!$user->isDirty()) {
-            return response()->json(['error' => 'You need to specify a different value to update ' , 'code' => 422], 422 );
+            return $this->ErrorResponse('You need to specify a different value to update ', 422);
+            //return response()->json(['error' => 'You need to specify a different value to update ' , 'code' => 422], 422 );
         }
 
         $user->save();
