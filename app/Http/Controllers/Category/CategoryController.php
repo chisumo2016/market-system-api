d8884e3246419342226ba537d7a0947303794256
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-01-31 15:33:45 +0000

    Implement the store method in controller using Model Binding

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -41,6 +41,14 @@ class CategoryController extends ApiController
     public function store(Request $request)
     {
         //
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+        ];
+        $this->validate($request, $rules);
+        $newCategory = Category::create($request->all());
+
+        return $this->showOne($newCategory, 201);
     }
 
     /**
