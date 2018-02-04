68647ef309602161793d451d7994c2fd2666f0e6
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-04 19:06:59 +0000

    Removing an image when creating ProductSellerController

--- app/Http/Controllers/Seller/sellerProductController.php
+++ app/Http/Controllers/Seller/sellerProductController.php
@@ -9,6 +9,7 @@ use App\User;
 use Illuminate\Foundation\Testing\HttpException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class sellerProductController extends ApiController
 {
@@ -140,7 +141,11 @@ class sellerProductController extends ApiController
     {
         //
         $this->checkSeller($seller, $product);
+
+
         $product->delete();
+
+        Storage::delete($product->image);
 
         return $this->showOne($product);
     }
