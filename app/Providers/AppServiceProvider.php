e567cea61fa618bd66fbcbcb1f98cfb37cfe513f
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-04 17:53:31 +0000

    Handling the Product Availability by Using Event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 
+use App\Product;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+
+        Product::updated(function($product){
+            if ($product->quantity == 0 && $product->isAvailable){
+                $product->status = Product::UNVAILABLE_PRODUCT;
+
+                $product->save();
+            }
+        });
 
     }
 
