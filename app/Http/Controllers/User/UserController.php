2cdb17bd0f365e4e57751dbe2ce3b3aa3d2ce477
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-06 00:43:22 +0000

    Implement the retry helper to deal with failing prone actions

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -183,7 +183,12 @@ class UserController extends ApiController
             return $this->errorResponse('This is user is already verified ', 409);
         }
 
-        Mail::to($user)->send(new UserCreated($user));
+
+        retry(5, function() use ($user) {
+            Mail::to($user)->send(new UserCreated($user));
+        },100);
+
+        //Mail::to($user)->send(new UserCreated($user));
 
         return $this->showMessage('The verification email has been resend');
     }
