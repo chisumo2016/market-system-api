e5ccfbc7635c7eb7376b7acf488d21180466789c
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-06 18:21:05 +0000

    Implementing HATEOAS Hypermedia Control for categoryTransformer

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -124,7 +124,7 @@ trait  ApiResponser
 
          ksort($queryParams);
          $queryString = http_build_query($queryParams);
-         
+
          $fullUrl =  "{$url} ? {$queryString}";
 
         return Cache::remember( $fullUrl, 30/60, function () use($data) {
