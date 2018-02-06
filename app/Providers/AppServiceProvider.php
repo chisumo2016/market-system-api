c9384b02c486c97041ea89448a65e07cc23ee16b
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-06 00:09:07 +0000

    Send  verification for email changed using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,11 @@ namespace App\Providers;
 
 
 use App\Mail\UserCreated;
+use App\Mail\UserMailChanged;
 use App\Product;
 use App\User;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 
+        User::created(function ($user) {
+            Mail::to($user)->send(new UserCreated($user));
+        });
+
+        User::updated(function ($user) {
+            if($user->isDirty('email')){
+                Mail::to($user)->send(new UserMailChanged($user));
+            }
+        });
 
         Product::updated(function($product){
             if ($product->quantity == 0 && $product->isAvailable){
@@ -30,9 +41,6 @@ class AppServiceProvider extends ServiceProvider
         });
 
 
-        User::created(function ($user) {
-            Mail::to($user)->send(new UserCreated($user));
-        });
 
     }
 
