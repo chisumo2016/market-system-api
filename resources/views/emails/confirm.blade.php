356f695ef416e9634835596208c1ec72e7f7eedf
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-06 01:09:45 +0000

    Implement the Laravel Markdown Mailables

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,5 +1,24 @@
-Hello {{ $user->name  }}
 
-You changed your email, so we nned to verify this new  address .Please use the link below
+@component('mail::message')
+    # Hello {{ $user->name  }}
 
-{{ route('verify', $user->verification_token) }}
+    You changed your email, so we nned to verify this new  address .Please use the button below
+
+    @component('mail::button', ['url' =>  route('verify', $user->verification_token)  ])
+       Verify Account
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
+
+
+
+
+
+{{--Hello {{ $user->name  }}--}}
+
+{{--You changed your email, so we nned to verify this new  address .Please use the link below--}}
+
+{{--{{ route('verify', $user->verification_token) }}--}}
+
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,22 @@
-Hello {{ $user->name  }}
-Thank you for create an account .Please verify your email using the link :
 
-{{ route('verify', $user->verification_token) }}
+
+
+@component('mail::message')
+    # Hello {{ $user->name  }}
+
+    Thank you for create an account .Please verify your email using the button :
+
+    @component('mail::button', ['url' => route('verify', $user->verification_token)  ])
+        Verify Account
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
+
+
+
+{{--Hello {{ $user->name  }}--}}
+{{--Thank you for create an account .Please verify your email using the link :--}}
+
+{{--{{ route('verify', $user->verification_token) }}--}}
