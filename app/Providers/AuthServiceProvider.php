fb56dccbbbc2fcbca67b8a1b6715777112b8f811
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-02-15 17:13:16 +0000

    Refreshing the Tokens by adding the method in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,6 +31,7 @@ class AuthServiceProvider extends ServiceProvider
         
         Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        Passport::enableImplicitGrant();
 
         //
     }
